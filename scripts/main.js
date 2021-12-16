23da472f459828b02fccd70ac2622110a9d319df
Diego Rubi <diegocrubi@gmail.com>
2021-12-16 06:43:21 -0600

    uploading alternative picture of sophie and adding a function to change pictures

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
 let myImage = document.querySelector('img'); 
 
 myImage.onclick = function(){
-    let
+    let mySrc = myImage.getAttribute('src'); 
+
+    if(mySrc === 'images/sophie-curled-up.jpg'){
+        myImage.setAttribute('src', 'images/sophie-curled-up.jpg'); 
+
+    }else{
+        myImage.setAttribute('src', 'images/sophie-on-desk.jpg')
+    }
+
 }

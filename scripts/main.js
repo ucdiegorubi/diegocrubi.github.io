08846457ca353ea33b49c5c59f9bc82b86e9d559
Diego Rubi <diegocrubi@gmail.com>
2021-12-16 07:12:26 -0600

    adding more interactivity

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,35 @@ let myImage = document.querySelector('img');
 myImage.onclick = function(){
     let mySrc = myImage.getAttribute('src'); 
 
-    if(mySrc === 'images/sophie-curled-up.jpg'){
+    if(mySrc === 'images/sophie-on-desk.jpg'){
         myImage.setAttribute('src', 'images/sophie-curled-up.jpg'); 
 
     }else{
         myImage.setAttribute('src', 'images/sophie-on-desk.jpg')
     }
 
+}
+
+let myButton = document.querySelector('button'); 
+let myHeading = document.querySelector('h1'); 
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}else{
+    let storedName = localStorage.getItem('name'); 
+    myHeading.textContent = 'Sophie is cool, ' + storedName; 
+}
+
+myButton.onclick = function(){
+    setUserName(); 
 }
